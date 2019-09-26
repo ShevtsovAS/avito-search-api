cf23b032e5355ec4eda9a6766271629f5759d15d
Alexandr Shevtsov <ashevtsov@luxoft.com>
2019-09-26 17:24:42 +0300

    Initial commit

---
+++ src/main/java/com/parser/avito/AvitoPareserApplication.java
@@ -0,0 +1,13 @@
+package com.parser.avito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvitoPareserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvitoPareserApplication.class, args);
+    }
+
+}
