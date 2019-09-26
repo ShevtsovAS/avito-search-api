7329b4a24e0ed8debc3f640e9af4dbe46649ec7d
Alexandr Shevtsov <ashevtsov@luxoft.com>
2019-09-26 17:35:53 +0300

    rename project

--- src/test/java/com/parser/avito/ApplicationTests.java
+++ src/test/java/com/parser/avito/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AvitoPareserApplicationTests {
+public class ApplicationTests {
 
     @Test
     public void contextLoads() {
